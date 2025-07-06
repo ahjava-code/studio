0026aa570664d3116b82557fd9412789e542f8f2
hp113 <hariprasad2315@gmail.com>
2025-07-06 12:00:18 +0530

    added pixel code to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,31 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {/* Existing Google Font links can be kept if preferred, or removed if using next/font */}
+        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        {/* <!-- Meta Pixel Code --> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1057016612691557');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript><img height="1" width="1" style={{ display: 'none' }}
+        src="https://www.facebook.com/tr?id=1057016612691557&ev=PageView&noscript=1"
+        /></noscript>
+        {/* <!-- End Meta Pixel Code --> */}
       </head>
       <body className={`${inter.variable} font-body antialiased`}>
         <ThemeProvider
